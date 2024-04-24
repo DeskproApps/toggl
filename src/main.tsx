71f8727a9af1888f71d6ac4d7e2d03d271397b01
ITweeno <itweeno@gmail.com>
2024-04-24 14:36:33 +0100

    add iframe resizer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { DeskproAppProvider } from "@deskpro/app-sdk";
+import "iframe-resizer/js/iframeResizer.contentWindow.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as Element);
 root.render(
